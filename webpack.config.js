cb690a38578dbf8530fb35068b3a0ed5732f9a7d
makaszy <m.a.kaszynski@protonmail.com>
2024-03-14 12:33:07 +0100

    Change dist to docs path

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     filename: 'main.js',
   },
   module: {
